2179f9bbb3c7a6da697abb1c25a9cbac99227007
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-08-13 00:05:55 -0300

    feat: Add Rotas básicas da aplicação.

--- src/server.js
+++ src/server.js
@@ -3,17 +3,38 @@ const fastify = require("fastify");
 const server = fastify();
 
 const courses = [
-  { id: 1, title: "Curso de Node.js" },
-  { id: 2, title: "Curso de React" },
-  { id: 3, title: "Curso de React Native" },
+  { id: "1", title: "Curso de Node.js" },
+  { id: "2", title: "Curso de React" },
+  { id: "3", title: "Curso de React Native" },
 ];
 
 server.get("/courses", () => {
-  return courses;
+  return { courses };
 });
 
-server.post("/courses", () => {
-  return courses;
+server.get("/courses/:id", (request, reply) => {
+  const courseId = request.params.id;
+
+  const course = courses.find((course) => course.id === courseId);
+
+  if (course) {
+    return { course };
+  }
+
+  return reply.status(404).send();
+});
+
+server.post("/courses", (request, reply) => {
+  const courseId = crypto.randomUUID();
+  const courseTitle = request.body.title;
+
+  if (!courseTitle) {
+    return reply.status(400).send({ message: "Título obrigatório." });
+  }
+
+  courses.push({ id: courseId, title: courseTitle });
+
+  return reply.status(201).send({ courseId });
 });
 
 server.listen({ port: 3333 }).then(() => {
