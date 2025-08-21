6330f882376334fde5e956257bb68e339a2aba07
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-08-20 22:26:23 -0300

    feat: Implementação da paginação.

--- src/routes/course/get-course.ts
+++ src/routes/course/get-course.ts
@@ -1,7 +1,7 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { db } from "../../database/client.ts";
 import { courses } from "../../database/schema.ts";
-import { asc, ilike } from "drizzle-orm";
+import { and, asc, ilike, SQL } from "drizzle-orm";
 import z from "zod";
 
 export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
@@ -12,8 +12,9 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
         tags: ["courses"],
         summary: "Recuperar todos os cursos",
         querystring: z.object({
-          search: z.string().optional(),
-        }), // sempre vai ser um objeto, e sempre vai ser optional
+          search: z.string().optional(), // sempre vai ser um objeto, e sempre vai ser optional
+          page: z.coerce.number().optional().default(1), //paginação
+        }),
         description: "Essa recupera todos os curso no BD",
         response: {
           200: z.object({
@@ -24,20 +25,33 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
                 description: z.string().nullable(),
               })
             ),
+            total: z.number(),
           }),
         },
       },
     },
     async (request, reply) => {
-      const { search } = request.query;
+      const { search, page } = request.query;
 
-      const result = await db
-        .select()
-        .from(courses)
-        .orderBy(asc(courses.title))
-        .where(search ? ilike(courses.title, `%${search}%`) : undefined);
+      const conditions: SQL[] = [];
 
-      return reply.send({ courses: result });
+      if (search) {
+        conditions.push(ilike(courses.title, `%${search}%`));
+      }
+
+      const [result, total] = await Promise.all([
+        db
+          .select()
+          .from(courses)
+          .orderBy(asc(courses.title))
+          .offset((page - 1) * 2)
+          .limit(2)
+          .where(and(...conditions)),
+
+        db.$count(courses, and(...conditions)), //qual a tabela que quero fz a contagem
+      ]); // as 2 querys vão executar ao mesmo tempo no BD
+
+      return reply.send({ courses: result, total });
     }
   );
 };
