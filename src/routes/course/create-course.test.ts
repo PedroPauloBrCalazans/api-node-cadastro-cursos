7f3a8b05fefa6a144b3e07bd2da8c6ed54187cc9
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-08-24 23:39:25 -0300

    feat: Add lib dotenv para o script de test + finalização da implementação do teste da rota de create.

--- src/routes/course/create-course.test.ts
+++ src/routes/course/create-course.test.ts
@@ -1,17 +1,24 @@
 import { test, expect } from "vitest";
 import request from "supertest";
 import { server } from "../../app.ts";
+import { faker } from "@faker-js/faker";
 
 test("Cria um curso com sucesso", async () => {
-  await server.ready();
+  await server.ready(); //espera o servidor está pronto, registrar todos os modulos
+
+  const title = faker.lorem.words(4);
+  const description = faker.lorem.words(3);
 
   const response = await request(server.server)
     .post("/courses")
     .set("Content-Type", "application/json")
-    .send({
-      title: "Curso de Vue.js",
-      description: "Curso completo de Vue.js",
-    });
+    .send({ title, description });
 
-  console.log(response.body);
+  console.log("Status:", response.status);
+  console.log("Body:", response.body);
+
+  expect(response.status).toEqual(201);
+  expect(response.body).toEqual({
+    courseId: expect.any(String),
+  });
 });
